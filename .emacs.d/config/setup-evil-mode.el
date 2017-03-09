d903196d10c89308e478b5aebc733e3b3ac9e79a
Luc Tielen <luc.tielen@gmail.com>
2017-03-09 08:26:54 +0100

    Add keybindings: escape now pretty much closes anything

--- .emacs.d/config/setup-evil-mode.el
+++ .emacs.d/config/setup-evil-mode.el
@@ -11,6 +11,26 @@
 	     (evil-leader/set-leader ",")
 	     (evil-leader/set-key-for-mode 'haskell-mode "t" 'haskell-mode-tag-find))
 
+;; Other key bindings
+
+;; Make escape exit pretty much anything
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In delete selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'minibuffer-keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
 ;; Fuzzy file finder in projects:
 (use-package fiplr
   :init
