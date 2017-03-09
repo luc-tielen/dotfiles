0a83f923efeeb883d72aa743b0c307b3e2f94d9a
Luc Tielen <luc.tielen@gmail.com>
2017-03-09 08:01:19 +0100

    Add fuzzy file finder

--- .emacs.d/config/setup-evil-mode.el
+++ .emacs.d/config/setup-evil-mode.el
@@ -10,5 +10,10 @@
 	     (global-evil-leader-mode)
 	     (evil-leader/set-leader ",")
 	     (evil-leader/set-key-for-mode 'haskell-mode "t" 'haskell-mode-tag-find))
-	     
+
+;; Fuzzy file finder in projects:
+(use-package fiplr
+  :init
+  (define-key evil-normal-state-map "\C-p" 'fiplr-find-file))
+
 (provide 'setup-evil-mode)
