85e1545367d4aa01e15cd5edab63f072c2e98a0e
Luc Tielen <luc.tielen@gmail.com>
2017-08-17 22:20:26 +0200

    Add minimal emacs julia config

--- .emacs.d/config/setup-julia.el
+++ .emacs.d/config/setup-julia.el
@@ -1,3 +1,13 @@
 
+(use-package julia-mode
+  :init
+  (add-to-list 'load-path "/usr/bin/julia"))
+
+(use-package flycheck-julia
+  :init
+  (flycheck-julia-setup))
+
+;; TODO bindings
+
 (provide 'setup-julia)
 
