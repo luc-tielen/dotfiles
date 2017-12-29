a2a428813fd45cd14600bbd6ed64a2867369d571
Luc Tielen <luc.tielen@gmail.com>
2017-12-29 09:12:02 +0100

    Configure alchemist for Elixir programming in emacs

--- .emacs.d/config/setup-elixir.el
+++ .emacs.d/config/setup-elixir.el
@@ -1,8 +1,15 @@
 
+;; TODO magit + evil-magit -> see chris mccords setup
+;; TODO mode line formatting -> see chris mccords setup
+;; TODO evil-surround for better wrapping of parens, "", etc
+
 ;; Activate Elixir mode
 (use-package elixir-mode)
 
 ;; Alchemist mode for better Elixir integration
-(use-package alchemist)
+(use-package alchemist
+  :config
+  (setq alchemist-goto-elixir-source-dir "~/.elixir/elixir"
+        alchemist-goto-erlang-source-dir "~/.erlang_otp/erlang"))
 
 (provide 'setup-elixir)
